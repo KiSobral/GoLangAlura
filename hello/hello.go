282f9248d3bae153c6a9e8c3d0bcd7a37328ec66
Hugo Sobral <hugosobral10@gmail.com>
2022-05-16 11:47:25 -0300

    Studies about go types

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	name := "Hugo"
+	var version float32 = 1.1
+	var age int = 22
+	fmt.Println("Hello mr.", name, "your age is", age)
+	fmt.Println("This program is under version", version)
+	fmt.Println("The type of var name is", reflect.TypeOf(name))
 }
