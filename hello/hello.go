5cb8ef0f6ed356051a14c2d328e6cde70c987291
Hugo Sobral <hugosobral10@gmail.com>
2022-05-16 13:55:05 -0300

    Start makin requisitions on web

--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,58 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"net/http"
+	"os"
 )
 
 func main() {
-	name := "Hugo"
-	var version float32 = 1.1
-	var age int = 22
-	fmt.Println("Hello mr.", name, "your age is", age)
-	fmt.Println("This program is under version", version)
-	fmt.Println("The type of var name is", reflect.TypeOf(name))
+	var command int
+	for {
+		command = menu()
+		controlCommand(command)
+	}
+}
+
+func menu() int {
+	var command int
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("0- Sair do Programa")
+	fmt.Scan(&command)
+	return command
+}
+
+func controlCommand(command int) {
+	switch command {
+	case 1:
+		monitorate()
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+
+	fmt.Println()
+	fmt.Println()
+}
+
+func monitorate() {
+	var sites [4]string
+	sites[0] = "https://random-status-code.herokuapp.com/"
+	sites[1] = "https://alura.com.br/"
+	sites[2] = "https://caelum.com.br/"
+	sites[3] = "https://aprender3.unb.br/"
+	fmt.Println("Monitorando...")
+	response, _ := http.Get(sites[3])
+
+	if response.StatusCode == 200 {
+		fmt.Println("Site:", sites[3], "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", sites[3], "está com problemas. Status Code:",
+			response.StatusCode)
+	}
 }
