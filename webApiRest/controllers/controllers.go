399deb429c7ea973263f1d8f576269a5f04b1b0e
Hugo Sobral <hugosobral10@gmail.com>
2022-05-18 17:25:00 -0300

    Finish REST CRUD

--- webApiRest/controllers/controllers.go
+++ webApiRest/controllers/controllers.go
@@ -26,5 +26,29 @@ func RetornaUmaPersonalidade(w http.ResponseWriter, r *http.Request) {
 	var personalidade models.Personalidade
 	database.DB.First(&personalidade, id)
 	json.NewEncoder(w).Encode(personalidade)
+}
 
+func CriaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	var personalidade models.Personalidade
+	json.NewDecoder(r.Body).Decode(&personalidade)
+	database.DB.Create(&personalidade)
+	json.NewEncoder(w).Encode(personalidade)
+}
+
+func DeletaPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.Delete(&personalidade, id)
+	json.NewEncoder(w).Encode(personalidade)
+}
+
+func EditarPersonalidade(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
+	json.NewDecoder(r.Body).Decode(&personalidade)
+	database.DB.Save(&personalidade)
+	json.NewEncoder(w).Encode(personalidade)
 }
--- webApiRest/routes/routes.go
+++ webApiRest/routes/routes.go
@@ -12,6 +12,9 @@ func HandleRequest() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", controllers.Home)
 	router.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	router.HandleFunc("/api/personalidades", controllers.CriaPersonalidade).Methods("Post")
 	router.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	router.HandleFunc("/api/personalidades/{id}", controllers.DeletaPersonalidade).Methods("Delete")
+	router.HandleFunc("/api/personalidades/{id}", controllers.EditarPersonalidade).Methods("Put")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
