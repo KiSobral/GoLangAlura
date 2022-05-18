0b51d6b672aeb10fbda73d3503fc320205d20a52
Hugo Sobral <hugosobral10@gmail.com>
2022-05-18 16:25:47 -0300

    Connect to database

--- webApiRest/main.go
+++ webApiRest/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"GoLangAlura/webApiRest/database"
 	"GoLangAlura/webApiRest/models"
 	"GoLangAlura/webApiRest/routes"
 	"fmt"
@@ -11,6 +12,7 @@ func main() {
 		{Id: 1, Nome: "Maria", Historia: "A maior bióloga do Brasil"},
 		{Id: 2, Nome: "Eduarda", Historia: "A maior artista do Brasil"},
 	}
+	database.ConnectToDatabase()
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
