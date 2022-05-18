b45a9ec06909f54acc5ee8904ea1988323fbe248
Hugo Sobral <hugosobral10@gmail.com>
2022-05-18 18:07:10 -0300

    Add middleware and allow CORS

--- webApiRest/routes/routes.go
+++ webApiRest/routes/routes.go
@@ -2,19 +2,22 @@ package routes
 
 import (
 	"GoLangAlura/webApiRest/controllers"
+	"GoLangAlura/webApiRest/middleware"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	router := mux.NewRouter()
+	router.Use(middleware.ContentTypeMiddleware)
 	router.HandleFunc("/", controllers.Home)
 	router.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	router.HandleFunc("/api/personalidades", controllers.CriaPersonalidade).Methods("Post")
 	router.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
 	router.HandleFunc("/api/personalidades/{id}", controllers.DeletaPersonalidade).Methods("Delete")
 	router.HandleFunc("/api/personalidades/{id}", controllers.EditarPersonalidade).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(router)))
 }
